7c05f580df25450cd6c924e5b748133fe53ec520
Cosmin Ursachi <cosmin.ursachi@gmail.com>
2018-01-22 02:32:58 +0200

    a new way of writting smart contracts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# smartBTC
+## SmartBTC sample smart contracts
+
+This repository will showcase the functionality of smartBTC.io and will mainly contain examples of **smart contracts.**
+ 
+## Examples of smart contracts:
+- twitter followers contract
+
+## The smartBTC platform
+- the motivation is to allow people to easily write and understand smart contracts
+- contracts are immutable (this means that a contract can not change until it is fulfilled or it expires)
+- the contracts are executed in a centralized manner, they do not reside on the blockchain 
---
+++ smart_contracts/TwitterFollowers.py
@@ -0,0 +1,25 @@
+# --------------- Dependencies ------------------------
+# The bellow line should be used in dependencies textbox
+# pip install requests
+
+# --------------- Actual Contract ---------------------
+import time
+import requests
+
+
+def contract():
+    nr_of_followers = 1000
+    print 'Inside Contract'
+    print 'Current time: ' + time.strftime("%Y-%m-%d %H:%M")
+    twitter_response = requests.get("https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=smartBtcIO").json();
+    count = twitter_response[0]['followers_count']
+    print 'current followers: ' + str(count)
+
+    if count > nr_of_followers:
+        print 'Contract done.'
+        return True
+    else:
+        print 'Contract NOT done! Still need an extra: {} followers'.format(nr_of_followers-count)
+        return False
+
+#contract()
